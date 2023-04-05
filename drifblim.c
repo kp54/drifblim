ed2102217ed39257dd3bb4bb24943438c9afccd6
kp54 <kangpang65@gmail.com>
2023-04-05 20:16:32 +0900

    sigwait and reap

--- drifblim.c
+++ drifblim.c
@@ -1,18 +1,34 @@
+#include<errno.h>
+#include<signal.h>
 #include<stdbool.h>
 #include<stdlib.h>
 #include<sys/wait.h>
-#include<errno.h>
 #include<unistd.h>
-
-#define INTERVAL 60u
 
 int
 main(void)
 {
+    int ret, signo;
+    sigset_t ss;
+
+    sigemptyset(&ss);
+    sigaddset(&ss, SIGCHLD);
+
+    sigprocmask(SIG_BLOCK, &ss, NULL);
+
     while(true) {
-        wait(NULL);
-        if (errno == ECHILD) {
-            sleep(INTERVAL);
+        ret = sigwait(&ss, &signo);
+        if (ret != 0) {
+            exit(EXIT_FAILURE);
+        }
+
+        if (signo == SIGCHLD) {
+            while (true) {
+                wait(NULL);
+                if (errno == ECHILD) {
+                    break;
+                }
+            }
         }
     }
 }
